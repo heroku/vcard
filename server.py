c189192b39fd628262ecc9b9645c66c9a8b082f9
James Ward <james@jamesward.org>
2012-06-13 02:06:40 -0600

    fix proxy server

--- server.py
+++ server.py
@@ -2,31 +2,26 @@ import os
 import SocketServer
 import SimpleHTTPServer
 import urllib
+import httplib
 import logging
-import httplib2
 
 PORT = int(os.environ.get("PORT", 5000))
-
-h = httplib2.Http()
 
 class Proxy(SimpleHTTPServer.SimpleHTTPRequestHandler):
     def do_POST(self):
         contentType = self.headers.getheader('content-type')
         length = int(self.headers.getheader('content-length'))
         data_string = self.rfile.read(length)
-        logging.error(self.headers)
-        logging.error(length)
-        logging.error(data_string)
-        logging.error(contentType)
         headers = {'Content-Type': 'application/json'}
-        resp, content = h.request("http://vcard-share.herokuapp.com/", "POST", body=data_string, headers=headers)
-        logging.error(resp)
-        logging.error(content)
-        self.send_response(int(resp['status']))
-        self.send_header('content-type', resp['content-type'])
-        self.send_header('content-length', resp['content-length'])
+        conn = httplib.HTTPConnection("vcard-share.herokuapp.com")
+        conn.request("POST", "/", data_string, headers)
+        response = conn.getresponse()
+        data = response.read()
+        self.send_response(response.status)
+        self.send_header('content-type', response.getheader('content-type'))
+        self.send_header('content-length', response.getheader('content-length'))
         self.end_headers()
-        self.wfile.write(content)
+        self.wfile.write(data)
 
 httpd = SocketServer.ForkingTCPServer(('', PORT), Proxy)
 print "Listending on port ", PORT
