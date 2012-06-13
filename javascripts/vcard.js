8db6e7257e1eb3e4cfbf14a0109cbfb551ed3a16
James Ward <james@jamesward.org>
2012-06-12 21:22:49 -0600

    init

---
+++ javascripts/vcard.js
@@ -0,0 +1,32 @@
+$(function() {
+
+  // `Flip card
+  $('.card').click(function(e) {    
+    if (!$(this).hasClass('create-card')) $(this).toggleClass('is-flipped');    
+  });
+
+  // `Call to create card action
+  $('#make-card').click(function(e) {
+    e.preventDefault();
+    $('#make').fadeOut('fast');
+    $('#content').removeClass('is-ready').addClass('hide');
+    $('#content-form').addClass('is-ready');
+  }); 
+
+  // `Cancel create vcard
+  $('.cancel').click(function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    $('#make').fadeIn('slow');
+    $('#content-form').removeClass('is-ready');
+    $('#content').removeClass('hide').addClass('is-ready');
+  });
+  
+  $('input, textarea').bind('focus blur', function () {
+    $(this).prev('label').toggleClass('focused');
+  });
+  $('input[name=OWNER_TITLE],input[name=OWNER_EMAIL],input[name=COMPANY_NAME],input[name=COMPANY_URL],textarea[name=COMPANY_ADDRESS]').bind('focus blur', function () {
+    $('label[for=OWNER_NAME]').toggleClass('focused');
+  });
+
+});
